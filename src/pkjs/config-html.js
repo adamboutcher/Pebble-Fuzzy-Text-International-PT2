84e7460dd70e129f1466af02f178c7b5d57e444a
Adam <adam.boutcher@gmail.com>
2026-07-06 22:44:39 +0100

    Rename Flemish picker label to native "Vlaams"
    Use the native name to match the other entries (Nederlands, Deutsch) and
    move it to the end of the list to keep the picker alphabetical by label.
    
    Co-authored-by: optio <1019030+optio@users.noreply.github.com>

--- src/pkjs/config-html.js
+++ src/pkjs/config-html.js
@@ -52,12 +52,12 @@ var configHTML = [
   '        <option value="en_GB">English (Great Britain)</option>',
   '        <option value="en_US">English (United States)</option>',
   '        <option value="es">Espa&#241;ol</option>',
-  '        <option value="nl_BE">Flemish</option>',
   '        <option value="fr">Fran&#231;ais</option>',
   '        <option value="nl">Nederlands</option>',
   '        <option value="no">Norsk</option>',
   '        <option value="pt">Portugu&#234;s</option>',
   '        <option value="sv">Svenska</option>',
+  '        <option value="nl_BE">Vlaams</option>',
   '      </select>',
   '    </fieldset>',
 
